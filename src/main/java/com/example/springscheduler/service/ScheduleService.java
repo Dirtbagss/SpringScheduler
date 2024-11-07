893d46305c54971a303cda6de3781f1679c23ce1
dirtbagss <dirtbagag4@gmail.com>
2024-11-07 19:08:01 +0900

    Init : ScheduleService

--- src/main/java/com/example/springscheduler/service/ScheduleService.java
+++ src/main/java/com/example/springscheduler/service/ScheduleService.java
@@ -1,6 +1,21 @@
 package com.example.springscheduler.service;
 
+import com.example.springscheduler.dto.ScheduleRequestDto;
+import com.example.springscheduler.dto.ScheduleResponseDto;
+
+import java.util.List;
+
 public interface ScheduleService {
+
+    ScheduleResponseDto saveSchedule(ScheduleRequestDto requestDto);
+
+    List<ScheduleResponseDto> findAllSchedules();
+
+    ScheduleResponseDto findScheduleById(Long Schedule);
+
+    ScheduleResponseDto updateSchedule(Long Schedule, String writer, String content);
+
+    void deleteSchedule(Long Schedule);
 
 
 }
