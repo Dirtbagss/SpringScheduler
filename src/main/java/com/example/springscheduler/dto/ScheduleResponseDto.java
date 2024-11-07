b1cc512ab9cd0c00d4d23a889bede04d1397ddfe
dirtbagss <dirtbagag4@gmail.com>
2024-11-07 19:04:38 +0900

    Init : ResponseDto

---
+++ src/main/java/com/example/springscheduler/dto/ScheduleResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.springscheduler.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleResponseDto {
+
+    private Long schedule;
+    private Long userId;
+    private String password;
+    private Date reg_date;
+    private String content;
+    private Date update_date;
+    private String writer;
+
+}
